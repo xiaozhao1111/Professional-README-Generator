7a0149ac0d9c5b58bf59189765f4f8c8c7cf46a3
xiaozhao1111 <zhaoxiao20071106@gmail.com>
2023-02-26 20:24:43 +0000

    question array added

--- index.js
+++ index.js
@@ -12,14 +12,52 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'Please give a short description of you project.',
+        message: 'Please give a short description of you project: ',
         name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'How to install the application?',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'How to use the application?',
+        name: 'usage'
+    },
+    {
+        type: 'list',
+        message: 'Please select the license: ',
+        choices: ['MIT License', 'Apache 2.0 License', 'The Unlicense', 'No license'],
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'What\'s the contributing guidelines for this project?',
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: 'Could you please leave your email address for Q&A?',
+        name:'email',
+        validate: function(email)
+        {
+            // Regex mail check (return true if valid mail)
+            return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+        }
+    },
+    {
+        type: 'input',
+        message: 'Please enter your Github username: ',
+        name: 'githubUserName',
     }
 ];
 
 // function to write README file
 function writeToFile(filename, data) {
-    fs.writeFile(`'${filename}.md'`, JSON.stringify(data), (err) => err && console.error(err))
+    fs.writeFile(`${filename}.md`, JSON.stringify(data), (err) => {
+        err ? console.error(err) : console.log('A new README file was saved!');
+    })
 }
 
 // function to initialize program
