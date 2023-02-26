5f2c47b2abcabec1d2cf0db47936fc74fd414561
xiaozhao1111 <zhaoxiao20071106@gmail.com>
2023-02-26 18:57:20 +0000

    sample questions for inquirer package tested

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        message: 'What\'s the tile of your project?',
+        name:'title'
+    },
+    {
+        type: 'input',
+        message: 'Please give a short description of you project.',
+        name: 'description'
+    }
 ];
 
 // function to write README file
@@ -14,7 +23,10 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions)
+        .then((response) => {
+            console.log(response);
+        })
 }
 
 // function call to initialize program
