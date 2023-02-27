c545b10a1a97f2ed3ef7403c06e6b8d57a5c483f
xiaozhao1111 <zhaoxiao20071106@gmail.com>
2023-02-27 19:11:38 +0000

    basic structure for the markdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,12 +3,17 @@ function generateMarkdown(data) {
   // Destructing the data object
   const {title, description, installation, usage, license, contributing, email, githubUserName} = data;
 
+  const githubURL = 'https://github.com/' + githubUserName;
   // create content table and link to specific sections
   const contentTable = `
 ## Table of Contents
 - [Description](#desc)
+- [Installation](#install)
 - [Usage Tips](#usage)
-
+- [Tests](#tests)
+- [Contributing](#contributing)
+- [Questions](#questions)
+- [License](#license)
 `
 
   return `
@@ -21,6 +26,29 @@ ${contentTable}
 
 ${description}
 
+<a name='install'></a>
+## Installation
+
+${installation}
+
+<a name='usage'></a>
+## Usage Tips
+
+${usage}
+
+<a name='tests'></a>
+## Tests
+
+<a name='contributing'></a>
+## Contributing
+
+${contributing}
+
+<a name='questions'></a>
+## Questions
+
+* If you have any doubts, feel free to contact me via ${email}
+* Find more about my works on [my Github](${githubURL})
 
 `;
 }
--- xiao.md
+++ xiao.md
@@ -4,13 +4,40 @@
 
 ## Table of Contents
 - [Description](#desc)
+- [Installation](#install)
 - [Usage Tips](#usage)
-
+- [Tests](#tests)
+- [Contributing](#contributing)
+- [Questions](#questions)
+- [License](#license)
 
 
 <a name='desc'></a>
 ## Description
 
-hi
+jhi
 
+<a name='install'></a>
+## Installation
+
+hihi
+
+<a name='usage'></a>
+## Usage Tips
+
+kljk
+
+<a name='tests'></a>
+## Tests
+
+<a name='contributing'></a>
+## Contributing
+
+dsafa
+
+<a name='questions'></a>
+## Questions
+
+* If you have any doubts, feel free to contact me via dasf@163.com
+* Find more about my works on [my Github](githubURL)
 
